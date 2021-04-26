b48bf8d29b37247301db9f40e9f9ec8b766e1438
chatchaijc <chatchaijc@gmail.com>
2021-04-26 21:49:44 +0700

    select  and switch value

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Import stylesheets
-import "./style.css";
-
-// Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-/////103destructuring_array
 ("use strict");
 const restaurant = {
   name: "Classico Italiano",
@@ -12,3 +5,19 @@ const restaurant = {
   categories: ["Focaccia", "Bruschetta", "Carlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"]
 };
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c);
+const [x, y, z] = arr;
+console.log(x, y, z);
+let [first,second] = restaurant.categories;
+console.log(first,second);
+[first, ,second] = restaurant.categories;
+console.log(first,second);
+//Switch value
+const temp = first;
+first = second;
+second = temp;
+console.log(first,second);
