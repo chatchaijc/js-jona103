2d36a6d5ebe03029b6b22dfa3b44da185828b751
chatchaijc <chatchaijc@gmail.com>
2021-04-26 21:02:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jona103
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jona103)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+/////103destructuring_array
+("use strict");
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, FFirenze, Italy",
+  categories: ["Focaccia", "Bruschetta", "Carlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"]
+};
