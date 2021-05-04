fb0785c316a6cf50cfb113c9aa843caedeef7228
chatchaijc <chatchaijc@gmail.com>
2021-05-04 16:29:45 +0700

    spread and rais operator

--- index.js
+++ index.js
@@ -33,10 +33,55 @@ const restaurant = {
 
   orderPasta:function(ing1,ing2,ing3){
     console.log(`Here is your declicious pasta with ${ing1},${ing2},${ing3}`);
-  }
+  },
+
+  orderPizza: function(mainIngredient,...otherIngredient){
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 
 };
 
+//1) Destructuring
+
+// Spread, because on Right side of =
+const arr = [1,2,...[3,4]];
+
+
+// Rest, because on Left side of =
+const [a,b, ...others] = [1,2,3,4,5];
+console.log(a,b,others);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza,risotto,otherFood);
+
+// objects
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);// sat is own objects
+
+//2) Functions
+
+const add = function(...numbers){
+  let sum = 0;
+  for(let i =0; i<numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+
+
+add(2,3);
+add(2,3,5,7);
+add(8,2,5,3,2,1,4);
+
+const x = [23,5,7];
+add(...x);
+
+restaurant.orderPizza('mushrooms','onion','olives','spinach');
+restaurant.orderPizza('spinach');
+
+/*
+/////////////////////////////////////////
+// The Spread Operator(...)
 const arr = [7,8,9];
 const badNewArr = [1,2,arr[0],arr[1],arr[2]];
 console.log(badNewArr);
@@ -91,9 +136,9 @@ const {
 } = openingHours;
 console.log(o, c);
 
-const ingredients = [prompt('Let\'s make pasta! Ingredient 1?'),prompt('Ingredient 2?'),prompt('Ingredient 3?')];
-console.log(ingredients);
-
+// const ingredients = [prompt('Let\'s make pasta! Ingredient 1?'),prompt('Ingredient 2?'),prompt('Ingredient 3?')];
+// console.log(ingredients);
+*/
 /*
 /////////////////////////////////////////////////
 // Destructuring Arrays
